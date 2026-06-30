c1d247fdfa815e3ddf8bbda5a34b95234f4da323
Samyak Chhajed <216376983+samyakchhajed@users.noreply.github.com>
2026-06-30 23:50:11 +0530

    Revert "Trying to break the pipeline"
    This reverts commit 1a059eca460bce55eb8ebee308c8474e01c8b046.

--- 2-ci_cd/01-first-github-actions-workflow/test_app.py
+++ 2-ci_cd/01-first-github-actions-workflow/test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.get_json() == {"message": "Hello, World!"}
+    assert response.get_json() == {"message": "Hello, DevOps!"}
